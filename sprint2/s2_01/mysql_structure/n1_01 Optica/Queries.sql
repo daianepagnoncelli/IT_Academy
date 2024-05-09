a5c7595a77b936ecfc4181d94afb4755b195dd22
daianepagnoncelli <124785634+daianepagnoncelli@users.noreply.github.com>
2024-05-09 06:04:49 +0200

    Update Queries.sql

--- sprint2/s2_01/mysql_structure/n1_01 Optica/Queries.sql
+++ sprint2/s2_01/mysql_structure/n1_01 Optica/Queries.sql
@@ -1,14 +1,12 @@
 -- Resources
 
 -- Lista el total de facturas de un cliente/a en un período determinado.
-
 SELECT customer_id, COUNT(*) AS total_invoices
 FROM Sales
 WHERE customer_id = 2 AND sale_date BETWEEN '2023-02-15' AND '2023-02-20'
 GROUP BY customer_id;
 
 -- Lista los diferentes modelos de gafas que ha vendido un empleado durante un año.
-
 SELECT DISTINCT Glasses.brand
 FROM Sales
 JOIN Glasses ON Sales.glass_id = Glasses.glass_id
@@ -16,3 +14,7 @@ JOIN Employees ON Sales.employee_id = Employees.employee_id
 WHERE Employees.employee_id = 5 AND YEAR(sale_date) = 2023;
 
 -- Lista a los diferentes proveedores que han suministrado gafas vendidas con éxito por la óptica.
+SELECT DISTINCT Suppliers.name
+FROM Sales
+JOIN Glasses ON Sales.glass_id = Glasses.glass_id
+JOIN Suppliers ON Glasses.supplier_id = Suppliers.supplier_id;
