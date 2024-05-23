9c13ccfac8048e7a98990bf2f7bc13ec371db8d7
daianepagnoncelli <124785634+daianepagnoncelli@users.noreply.github.com>
2024-05-23 19:40:57 +0200

    Update Queries.sql

--- sprint2/s2_01/mysql_structure/n1_01 Optica/Queries.sql
+++ sprint2/s2_01/mysql_structure/n1_01 Optica/Queries.sql
@@ -1,20 +1,25 @@
 -- Resources
 
 -- Lista el total de facturas de un cliente/a en un período determinado.
-SELECT customer_id, COUNT(*) AS total_invoices
-FROM Sales
-WHERE customer_id = 2 AND sale_date BETWEEN '2023-02-15' AND '2023-02-20'
-GROUP BY customer_id;
+SELECT customers.name, SUM(glasses.price) AS "totalInvoices"
+FROM opticsdb.purchases
+INNER JOIN opticsdb.glasses ON purchases.idGlasses = glasses.glassesId
+JOIN opticsdb.customers ON purchases.idCustomer = customers.customerId
+WHERE purchases.purchaseDate BETWEEN '2023-01-01' AND '2023-12-31'
+GROUP BY purchases.idCustomer;
+
 
 -- Lista los diferentes modelos de gafas que ha vendido un empleado durante un año.
-SELECT DISTINCT Glasses.brand
-FROM Sales
-JOIN Glasses ON Sales.glass_id = Glasses.glass_id
-JOIN Employees ON Sales.employee_id = Employees.employee_id
-WHERE Employees.employee_id = 5 AND YEAR(sale_date) = 2023;
+SELECT DISTINCT glasses.*
+FROM opticsdb.purchases
+INNER JOIN opticsdb.glasses ON purchases.idGlasses = glasses.glassesId
+JOIN opticsdb.sellers ON purchases.idSeller = sellers.sellerId
+WHERE sellers.name = 'Peter' AND purchases.purchaseDate BETWEEN '2024-01-01' AND '2024-12-31';
+
 
 -- Lista a los diferentes proveedores que han suministrado gafas vendidas con éxito por la óptica.
-SELECT DISTINCT Suppliers.name
-FROM Sales
-JOIN Glasses ON Sales.glass_id = Glasses.glass_id
-JOIN Suppliers ON Glasses.supplier_id = Suppliers.supplier_id;
+SELECT DISTINCT suppliers.name, suppliers.address, suppliers.phone
+FROM opticsdb.purchases
+INNER JOIN opticsdb.glasses ON purchases.idGlasses = glasses.glassesId
+JOIN opticsdb.brands ON glasses.idBrand = brands.brandId
+JOIN opticsdb.suppliers ON brands.idSupplier = suppliers.supplierId;
