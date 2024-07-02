7aa701844e58a97a3ff9a0775609316c2cc5bc3a
Daiane Pagnoncelli <d_pagnoncelli@hotmail.com>
2024-07-02 11:13:08 +0200

    First Commit - Sprint4 - 2

---
+++ spring4/S04T01N02PagnoncelliDaiane/src/main/java/cat/itacademy/barcelonactiva/pagnoncelli/daiane/s04/t01/n02/S04T01N02PagnoncelliDaiane/Controllers/HelloWorldController.java
@@ -0,0 +1,24 @@
+package cat.itacademy.barcelonactiva.pagnoncelli.daiane.s04.t01.n02.S04T01N02PagnoncelliDaiane.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/HelloWorld")
+    public String greets(@RequestParam(defaultValue = "UNKNOWN") String name) {
+        return "Hello, " + name + ". You are running a Gradle project";
+    }
+
+    @GetMapping({"/HelloWorld2", "/HelloWorld2/{name}"})
+    public String greets2(@PathVariable(required = false) String name) {
+        if (name == null) {
+            name = "UNKNOWN";
+        }
+        return "Hello, " + name + ". You are running a Gradle project";
+    }
+}
+
