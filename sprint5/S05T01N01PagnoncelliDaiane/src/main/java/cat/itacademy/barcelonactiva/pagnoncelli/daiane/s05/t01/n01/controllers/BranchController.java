ff055b876debdcaf17f82f364086c9ebe12af547
Daiane Pagnoncelli <d_pagnoncelli@hotmail.com>
2024-07-09 12:01:40 +0200

    Commit Sprint 5 - various modifications

--- sprint5/S05T01N01PagnoncelliDaiane/src/main/java/cat/itacademy/barcelonactiva/pagnoncelli/daiane/s05/t01/n01/controllers/BranchController.java
+++ sprint5/S05T01N01PagnoncelliDaiane/src/main/java/cat/itacademy/barcelonactiva/pagnoncelli/daiane/s05/t01/n01/controllers/BranchController.java
@@ -3,46 +3,56 @@ package cat.itacademy.barcelonactiva.pagnoncelli.daiane.s05.t01.n01.controllers;
 import cat.itacademy.barcelonactiva.pagnoncelli.daiane.s05.t01.n01.model.dto.BranchDTO;
 import cat.itacademy.barcelonactiva.pagnoncelli.daiane.s05.t01.n01.model.services.BranchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/branch")
 public class BranchController {
 
     @Autowired
     private BranchService branchService;
 
-    @PostMapping(path = "/add")
-    public ResponseEntity<BranchDTO> saveBranch(@RequestBody BranchDTO branchDto) {
-        branchService.saveBranch(branchDto);
-        return new ResponseEntity<>(branchDto, HttpStatus.CREATED);
+    @GetMapping
+    public String viewBranches(Model model) {
+        List<BranchDTO> branchList = branchService.getAllBranches();
+        model.addAttribute("branchList", branchList);
+        model.addAttribute("branch", new BranchDTO());
+        return "branchForm";
     }
 
-    @PutMapping(path = "/update/{id}")
-    public ResponseEntity<BranchDTO> updateBranch(@PathVariable int id, @RequestBody BranchDTO branchDto) {
-        return new ResponseEntity<>(branchService.updateBranch(id, branchDto), HttpStatus.OK);
+    @PostMapping("/add")
+    public String addBranch(@ModelAttribute("branch") BranchDTO branchDTO) {
+        branchService.saveBranch(branchDTO);
+        return "redirect:/branch";
     }
 
-    @DeleteMapping(path = "/delete/{id}")
-    public ResponseEntity<String> deleteBranch(@PathVariable int id) {
+    @GetMapping("/edit/{id}")
+    public String editBranch(@PathVariable int id, Model model) {
+        BranchDTO branch = branchService.getOneBranch(id);
+        model.addAttribute("branch", branch);
+        return "editBranch";
+    }
+
+    @PostMapping("/update")
+    public String updateBranch(@ModelAttribute("branch") BranchDTO branchDTO) {
+        branchService.updateBranch(branchDTO.getPk_BranchID(), branchDTO);
+        return "redirect:/branch";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBranch(@PathVariable int id) {
         branchService.deleteBranch(id);
-        return new ResponseEntity<>("Branch id " + id + " deleted successfully", HttpStatus.OK);
-    }
-
-    @GetMapping(path = "/getOne/{id}")
-    public ResponseEntity<BranchDTO> getOneBranch(@PathVariable int id) {
-        return new ResponseEntity<>(branchService.getOneBranch(id), HttpStatus.OK);
-    }
-
-    @GetMapping(path = "/getAll")
-    public ResponseEntity<List<BranchDTO>> getAllBranches() {
-        return new ResponseEntity<>(branchService.getAllBranches(), HttpStatus.OK);
+        return "redirect:/branch";
     }
 }
+
+
+
+
 
 
 
---
+++ sprint5/S05T01N02PagnoncelliDaiane/src/main/java/cat/itacademy/barcelonactiva/pagnoncelli/daiane/s05/t01/n02/controllers/FlorController.java
@@ -0,0 +1,4 @@
+package cat.itacademy.barcelonactiva.pagnoncelli.daiane.s05.t01.n02.controllers;
+
+public class FlorController {
+}
