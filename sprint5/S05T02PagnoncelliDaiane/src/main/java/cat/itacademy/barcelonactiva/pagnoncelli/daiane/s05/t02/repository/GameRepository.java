2e6651e911949c66d0257fa1400206f2735bb0cb
Daiane Pagnoncelli <d_pagnoncelli@hotmail.com>
2024-07-15 13:05:23 +0200

    First Commit

--- sprint5/S05T02PagnoncelliDaiane/src/main/java/cat/itacademy/barcelonactiva/pagnoncelli/daiane/s05/t02/repository/GameRepository.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.pagnoncelli.daiane.s05.t02.repository;
-
-import cat.itacademy.barcelonactiva.pagnoncelli.daiane.s05.t02.model.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GameRepository extends JpaRepository<Game, Long> {
-}
