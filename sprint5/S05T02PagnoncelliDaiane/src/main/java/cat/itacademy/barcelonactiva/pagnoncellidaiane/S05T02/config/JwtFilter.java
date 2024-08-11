0d6693a993e86524817957ccd1f964c0ea220e51
Daiane Pagnoncelli <d_pagnoncelli@hotmail.com>
2024-08-11 11:20:55 +0200

    Add Sprint5 - DiceGame_up10-security

--- sprint5/S05T02PagnoncelliDaiane/src/main/java/cat/itacademy/barcelonactiva/pagnoncellidaiane/S05T02/config/JwtFilter.java
+++ sprint5/S05T02PagnoncelliDaiane/src/main/java/cat/itacademy/barcelonactiva/pagnoncellidaiane/S05T02/config/JwtFilter.java
@@ -27,7 +27,7 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
+        final String authHeader = request.getHeader("Authorization ");
         String username = null;
         String jwt = null;
 
